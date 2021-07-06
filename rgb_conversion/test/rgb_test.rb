26a2aaa1eb8fd64fcbe60f947e23847b88829ce8
mi-1109 <meisuzuki.sophia@gmail.com>
2021-07-06 20:53:38 +0900

    updated to_hex method

--- rgb_conversion/test/rgb_test.rb
+++ rgb_conversion/test/rgb_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
+require './lib/rgb'
 
-class RgbTest < Minitest::RgbTest
+class RgbTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
   end
